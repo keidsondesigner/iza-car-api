a7343ce5ac679a21c6100e6d7286d125290496cb
Keidson Roby <keidsondesigner@gmail.com>
2024-06-04 18:54:35 -0400

    Initial commit

---
+++ src/main/java/com/iza/tech/iza_car_api/IzaCarApiApplication.java
@@ -0,0 +1,13 @@
+package com.iza.tech.iza_car_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IzaCarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IzaCarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iza/tech/iza_car_api/IzaCarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iza.tech.iza_car_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IzaCarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
